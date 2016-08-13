7cd74ce6df241d6bd6976cd9f686d7d21c299d3c
shure <dd_dreams@163.com>
2016-08-13 13:46:39 +0800

    add gulp-plumber don`t stop server plugins

--- gulpfile.js
+++ gulpfile.js
@@ -14,13 +14,15 @@ var gulp = require('gulp'),
     babel = require("gulp-babel"),
     es2015 = require("babel-preset-es2015"),
     less = require('gulp-less'),
+    plumber = require('gulp-plumber'),
     port = process.env.port || 5000;
 
 gulp.task('browserify',function(){
-	gulp.src('./src/js/main.js')
+	gulp.src(['./src/js/main.js'])
 	.pipe(browserify({
 		transform: 'reactify',
 	}))
+	.pipe(plumber()) 
 	.pipe(gulp.dest('./dist/js'))
 });
 
@@ -34,32 +36,40 @@ gulp.task('connect',function(){
 
 gulp.task('js',function(){
 	gulp.src('./dist/**/*.js')
+	.pipe(plumber()) 
 	.pipe( connect.reload() )
 });
 
 gulp.task('html',function(){
 	gulp.src('./src/**/*.html')
+	.pipe(plumber()) 
 	.pipe( connect.reload() )
 });
 
 // es6 转 es5
 gulp.task('es6', function(){
 	gulp.src('./src/es6/**/*.js')
+	.pipe(plumber()) 
 	.pipe(babel({presets: [es2015]}))
-	.pipe(gulp.dest('./dist/js'));
+	.pipe(gulp.dest('./dist/js'))
+	.pipe( connect.reload() );
 });
 
 // less 转 css
 gulp.task('less', function(){
 	gulp.src('./src/less/**/*.less')
+	.pipe(plumber()) 
 	.pipe(less())
-	.pipe(gulp.dest('./dist/css'));
+	.pipe(gulp.dest('./dist/css'))
+	.pipe( connect.reload() );
 });
 
 gulp.task('watch',function(){
 	gulp.watch('./dist/**/*.js',['js']);
 	gulp.watch('./src/**/*.html',['html']);
-	gulp.watch('./src/js/**/*.js',['browserify']);
+	gulp.watch('./src/less/**/*.less',['less']);
+	gulp.watch('./src/es6/**/*.js',['es6']);
+	gulp.watch('./src/js/**/*.js',['browserify']);	
 });
 
 
--- src/js/component/App.js
+++ src/js/component/App.js
@@ -1,8 +1,15 @@
 var React = require('react');
 
 var Hello = React.createClass({
+	handleClick: function(){
+		location.href = '../src/demo.html';
+	},
 	render: function(){
-		return (<div>Hello world!</div>)
+		return (
+			<div>Hello world! 
+				<button onClick={this.handleClick}>go</button>
+			</div>
+		)
 	}
 });
 
